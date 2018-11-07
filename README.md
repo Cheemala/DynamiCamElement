a789e361765d2c95c50b253047e945eef20abf8a
Cheemala <chaits.net@gmail.com>
2018-11-07 22:55:36 +0530

    My first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DynamicCamElement
-This project lets you create dynamic image view and buttons to add &amp; click pictures. For more info .... dive into my project !!!!
---
+++ app/src/main/java/cheemala/projects/dynamicamera/HomeActivity.java
@@ -0,0 +1,124 @@
+package cheemala.projects.dynamicamera;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import static android.Manifest.permission.CAMERA;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public static int CAM_PERMISSION_REQUEST_CODE = 111;
+    public static int CAM_REQUEST_CODE = 222;
+    private LinearLayout dynamicItmHolder;
+    private LayoutInflater inflater;
+    private String currntImgTag;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initializeViews();
+        adDynamicItem();
+
+        if (!checkPermission()) {
+            requestPermission();
+        }
+    }
+
+    private void initializeViews() {
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        dynamicItmHolder = findViewById(R.id.dynamic_itm_holder);
+
+    }
+
+    private void adDynamicItem() {
+
+        final View itemView = inflater.inflate(R.layout.dynamic_cam_item, null);
+        ImageView imgVw = itemView.findViewById(R.id.img_vw);
+        imgVw.setTag(getImgTagIdntfer(dynamicItmHolder));
+        Button clkBtn = itemView.findViewById(R.id.clk_btn);
+        clkBtn.setTag(getCamBtnTagIdntfer(dynamicItmHolder));
+        Button adBtn = itemView.findViewById(R.id.ad_btn);
+        adBtn.setTag(getAdBtnTagIdntfer(dynamicItmHolder));
+        adBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adDynamicItem();
+            }
+        });
+
+        clkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String crnBtnTag = (String) view.getTag();
+                currntImgTag = "img" + crnBtnTag.substring(7);
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAM_REQUEST_CODE);
+                Toast.makeText(HomeActivity.this, "" + currntImgTag, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dynamicItmHolder.addView(itemView);
+        // Toast.makeText(HomeActivity.this,""+dynamicItmHolder.getChildCount(),Toast.LENGTH_SHORT).show();
+
+    }
+
+    private Object getAdBtnTagIdntfer(LinearLayout dynamicItmHolder) {
+        return "ad_btn" + (dynamicItmHolder.getChildCount() + 1);
+    }
+
+    private Object getCamBtnTagIdntfer(LinearLayout dynamicItmHolder) {
+        return "cam_btn" + (dynamicItmHolder.getChildCount() + 1);
+    }
+
+    private Object getImgTagIdntfer(LinearLayout dynamicItmHolder) {
+        return "img" + (dynamicItmHolder.getChildCount() + 1);
+    }
+
+    private boolean checkPermission() {
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+        return (result1 == PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA}, CAM_PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAM_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Toast.makeText(HomeActivity.this, "" + currntImgTag, Toast.LENGTH_SHORT).show();
+                ImageView dynamicImg = dynamicItmHolder.findViewWithTag(currntImgTag);
+                Bitmap bitmapImg = (Bitmap) data.getExtras().get("data");
+                dynamicImg.setImageBitmap(bitmapImg);
+            }
+        } else {
+            Toast.makeText(HomeActivity.this, "Image not captured!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
